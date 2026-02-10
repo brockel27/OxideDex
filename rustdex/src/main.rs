6fc9e01e133b594b8e036b1e95332335e28722c7
Brocke <wandering8350@gmail.com>
2026-02-09 21:37:02 -0800

    added main.rs and pokemon.rs

--- rustdex/src/main.rs
+++ rustdex/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+mod pokemon;
+
+use pokemon::{fetch_pokemon_data, Pokemon};
+use std::env;
+use tokio;
+
+async fn display_pokemon_data(pokemon_name: &str) {
+   match fetch_pokemon_data(pokemon_name).await {
+      Ok(pokemon) => {
+         println!("Name: {}", pokemon.name);
+         println!("Height: {}", pokemon.height);
+         println!("Weight: {}", types_to_string(&pokemon.types));
+         println!("Types: {}", types_to_string(&pokemon.types));
+      }
+      Err(error) => {
+         eprintln!("Error: {}", error);
+      }
+   }
 }
+
+fn types_to_string(types: &[pokemon::PokemonType]) -> String {
+   types
+      .iter()
+      .map(|ptype| ptype.type_data.name.as_str())
+      .collect::<Vec<_>>()
+      .join(", ")
+}
+
+#[tokio::main]
+async fn main() {
+   let args: Vec<String> = env::args().collect();
+   if args.len() == 2 {
+      let pokemon_name = &args[1];
+      display_pokemon_data(pokemon_name).await;
+   } else {
+      eprintln!("Usage: pokeapi_rust <pokemon_name>");
+   }
+}
+
+
