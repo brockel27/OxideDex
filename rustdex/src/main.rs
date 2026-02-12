6c8cb5bab50eab32ee5aba8a41fe0b052f5407b9
Brocke <wandering8350@gmail.com>
2026-02-12 14:36:17 -0800

    fixed some formatting

--- rustdex/src/main.rs
+++ rustdex/src/main.rs
@@ -1,40 +1,48 @@
-mod pokemon;
-
-use pokemon::{fetch_pokemon_data, Pokemon};
+use rustemon::client::RustemonClient;
+use rustemon::model::pokemon::Pokemon;
+use rustemon::pokemon::pokemon;
 use std::env;
-use tokio;
 
-async fn display_pokemon_data(pokemon_name: &str) {
-   match fetch_pokemon_data(pokemon_name).await {
-      Ok(pokemon) => {
-         println!("Name: {}", pokemon.name);
-         println!("Height: {}", pokemon.height);
-         println!("Weight: {}", types_to_string(&pokemon.types));
-         println!("Types: {}", types_to_string(&pokemon.types));
-      }
-      Err(error) => {
-         eprintln!("Error: {}", error);
-      }
-   }
+async fn display_pokemon_data(pokemon_name: &str, client: &RustemonClient) {
+    // Rustemon's get_by_name handles the API request and JSON parsing
+    match pokemon::get_by_name(pokemon_name, client).await {
+        Ok(p) => {
+            // Convert the integers from the API to floats
+            // Height orignally in decimeters (dm), Weight in hectograms (hg)
+            let height_in_meters = p.height as f32 / 10.0;
+            let weight_in_kg = p.weight as f32 / 10.0;
+            println!("Name: {}", p.name);
+            println!("Height: {} m", height_in_meters);
+            println!("Weight: {} kg", weight_in_kg);
+            println!("Types: {}", types_to_string(&p));
+        }
+        Err(error) => {
+            eprintln!("Error: Could not find '{}'. ({})", pokemon_name, error);
+        }
+    }
 }
 
-fn types_to_string(types: &[pokemon::PokemonType]) -> String {
-   types
-      .iter()
-      .map(|ptype| ptype.type_data.name.as_str())
-      .collect::<Vec<_>>()
-      .join(", ")
+fn types_to_string(pokemon: &Pokemon) -> String {
+    pokemon
+        .types
+        .iter()
+        .map(|ptype| ptype.type_.name.as_str())
+        .collect::<Vec<_>>()
+        .join(", ")
 }
 
 #[tokio::main]
 async fn main() {
-   let args: Vec<String> = env::args().collect();
-   if args.len() == 2 {
-      let pokemon_name = &args[1];
-      display_pokemon_data(pokemon_name).await;
-   } else {
-      eprintln!("Usage: pokeapi_rust <pokemon_name>");
-   }
+    // std::env::args() doen't allow access to uninitialized memory
+    let args: Vec<String> = env::args().collect();
+
+    // Initialize the client
+    let client = RustemonClient::default();
+
+    if args.len() == 2 {
+        let pokemon_name = args[1].to_lowercase();
+        display_pokemon_data(&pokemon_name, &client).await;
+    } else {
+        eprintln!("Usage: rustdex <pokemon_name_or_id>");
+    }
 }
-
-
